85435a708c4f3ccaa35fe4ab39200108f54a520f
vamsikrishnamangu <mvvk96@gmail.com>
2023-09-18 14:21:36 +0530

    feat: qr scanner update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="/qrScanner.js" async defer></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

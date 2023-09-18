4492965f83d5ff44302350a79b67ed4df768577b
vamsikrishnamangu <mvvk96@gmail.com>
2023-09-18 15:21:42 +0530

    feat: re optimized using ref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,60 @@
 //@ts-nocheck
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 export default function Home() {
+  const readerRef = useRef(null);
+  const resultRef = useRef(null);
+  const showRef = useRef(null);
   useEffect(() => {
     let scannerStarted = false;
-    const html5Qrcode = new Html5Qrcode("reader");
+    let readerElement = readerRef.current;
+    if (readerElement) {
+      const html5Qrcode = new Html5Qrcode(readerElement.id);
 
-    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-      if (decodedText) {
-        document.getElementById("show").style.display = "block";
-        document.getElementById("result").textContent = decodedText;
-        html5Qrcode.stop().catch((error) => console.error(error));
-      }
-    };
+      const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        if (decodedText && showRef.current && resultRef.current) {
+          showRef.current.style.display = "block";
+          resultRef.current.href = decodedText;
+          resultRef.current.textContent = decodedText;
+          html5Qrcode.stop().catch((error) => console.error(error));
+        }
+      };
 
-    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+      const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-    html5Qrcode
-      .start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
-      .then(() => {
-        scannerStarted = true;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-    // Cleanup on component unmount
-    return () => {
-      if (scannerStarted) {
-        html5Qrcode.stop().catch((err) => {
+      html5Qrcode
+        .start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
+        .then(() => {
+          scannerStarted = true;
+        })
+        .catch((err) => {
           console.error(err);
-          alert("Error starting the scanner: " + err.message); // Display error to users
         });
-      }
-      const readerDiv = document.getElementById("reader");
-      if (readerDiv) {
-        readerDiv.innerHTML = ""; // Clear the inner contents of the reader div
-      }
-    };
-  }, []);
 
+      return () => {
+        if (scannerStarted) {
+          html5Qrcode.stop().catch((err) => {
+            console.error(err);
+            alert("Error starting the scanner: " + err.message);
+          });
+        }
+        if (readerElement) {
+          readerElement.innerHTML = "";
+        }
+      };
+    }
+  }, []);
   return (
     <>
-      <p>scan the qr code</p>
-      <div style={{ textAlign: "center" }}>
-        <div id="reader" style={{ width: "500px" }}></div>
-        <div id="show" style={{ display: "none" }}>
-          <h4>Scanned Result</h4>
-          <p style={{ color: "blue" }} id="result"></p>
+      <>
+        <p>Scan the QR code</p>
+        <div style={{ textAlign: "center" }}>
+          <div id="reader" ref={readerRef} style={{ width: "500px" }}></div>
+          <div ref={showRef} style={{ display: "none" }}>
+            <h4>Scanned Result</h4>
+            <a style={{ color: "blue" }} ref={resultRef}></a>
+          </div>
         </div>
-      </div>
+      </>
     </>
   );
 }
